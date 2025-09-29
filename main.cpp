d6a1cf801bbdf627aa7aebf6b56665a826d8edc5
Максим Джамалян ип-511 <dzamalanmaksim@gmail.com>
2025-09-29 22:33:03 +0700

    Исправлено приветствие

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@
 int main(int argc, char** argv) {
     char* str = (char*)malloc(sizeof(char) * SIZE);
     printf("Введите своё имя:\n");
+ HEAD
     str = gets(str);
     printf("Привет, %s", str);
+
+    scanf("%s", str);
+    puts("Привет");
+puts(str);
+ 274c1ef (Исправлено приветствие)
     return 0;
 }
